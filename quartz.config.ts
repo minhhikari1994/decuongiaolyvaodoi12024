133f3e8141baf1760ba791a1f165891163156cb9
Minh Hikari <minhhikari1994+dev@outlook.com>
2025-01-04 13:12:24 +0700

    optimize navigation + upgrade node

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Giáo Lý Vào Đời",
     pageTitleSuffix: "",
-    enableSPA: false,
+    enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
